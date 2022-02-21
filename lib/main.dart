c80b926228a7a5b9a776fbeb7f4d6023b103de28
sjtsh <74091619+sjtsh@users.noreply.github.com>
2022-02-21 14:58:22 +0545

    this

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:psr_application/StateManagement/Counter.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 void main() {
-  runApp(const MyApp());
+  FirebaseApp secondaryApp = Firebase.app('psrapplication');
+  firebase_storage.FirebaseStorage storage =
+  firebase_storage.FirebaseStorage.instanceFor(app: secondaryApp);
+  firebase_storage.Reference ref =
+  firebase_storage.FirebaseStorage.instance.ref('/hey.jpg');
+  print(ref.fullPath);
+  print(ref.getDownloadURL());
+  // runApp(MultiProvider(providers: [
+  //   ChangeNotifierProvider(
+  //     create: (_) => Counter(),
+  //   ),
+  // ], child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -29,63 +44,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: context.watch<Counter>().currentScreenWidget,
+          ),
+          Container(
+            height: 60,
+            color: Colors.green,
+            child: Row(
+                children: [Icons.add, Icons.clear, Icons.cancel]
+                    .asMap()
+                    .entries
+                    .map((e) => Expanded(
+                            child: GestureDetector(
+                          onTap: () => context.read<Counter>().increment(e.key),
+                          child: Center(
+                            child: Icon(
+                              e.value,
+                              color: context.watch<Counter>().currentScreen ==
+                                      e.key
+                                  ? Colors.white
+                                  : Colors.white.withOpacity(0.5),
+                            ),
+                          ),
+                        )))
+                    .toList()),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
