c51a042a6b5fc1a926b51979e5a5e898f4dce3e0
popularkoju <webpk144@gmail.com>
2022-03-16 14:12:38 +0545

    no order

--- lib/Screens/OrderScreen/ConfirmOrderScreen/ConfirmOrder.dart
+++ lib/Screens/OrderScreen/ConfirmOrderScreen/ConfirmOrder.dart
@@ -37,38 +37,59 @@ class ConfirmOrder extends StatelessWidget {
                 ],
               ),
             ),
-            Container(
-              margin: EdgeInsets.all(12),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.black.withOpacity(0.1),
-                      offset: Offset(0, 2),
-                      blurRadius: 3)
-                ],
-              ),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 12,
-                  ),
-                  ...context
-                      .read<OrderScreenManagement>()
-                      .noOrderReasons
-                      .entries
-                      .map(
-                        (e) => Text(e.key.name.toString()),
-                      )
-                      .toList(),
-                  Text(context
-                      .read<OrderScreenManagement>()
-                      .noOrderReasons
-                      [context
-                      .read<OrderScreenManagement>()
-                      .noOrderReasons.keys.toList()[2]]!),
-                ],
+            context.read<OrderScreenManagement>().noOrderReasons.isEmpty? Expanded(child: Container(child: Center(child: Text("No Order Reason Added"),),)) : Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        offset: Offset(0, 2),
+                        blurRadius: 3)
+                  ],
+                ),
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: context.read<OrderScreenManagement>().noOrderReasons.length,
+                  itemBuilder: (_, index) {
+                    var data = context
+                        .read<OrderScreenManagement>()
+                        .noOrderReasons
+                        .entries
+                        .map(
+                          (e) => e.key.name)
+
+                        .toList();
+                   return  Padding(
+                     padding: const EdgeInsets.all(12.0),
+                     child: Column(
+                       crossAxisAlignment: CrossAxisAlignment.start,
+                       children:
+                       [
+                         // ...context
+                         //     .read<OrderScreenManagement>()
+                         //     .noOrderReasons
+                         //     .entries
+                         //     .map(
+                         //       (e) => Text(e.key.name.toString(),style: TextStyle(fontWeight: FontWeight.bold)),
+                         //     )
+                         //     .toList(),
+                         Text(data[index].toString() , style: TextStyle(fontWeight:FontWeight.bold),),
+                         SizedBox(height: 12,),
+                         Text(context
+                             .read<OrderScreenManagement>()
+                             .noOrderReasons
+                         [context
+                             .read<OrderScreenManagement>()
+                             .noOrderReasons.keys.toList()[index]]! , style: TextStyle(color: Colors.grey),),
+                       ],
+                     ),
+                   );
+                  },
+                ),
               ),
             ),
           ],
